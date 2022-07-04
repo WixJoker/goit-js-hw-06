0b9e6ae659141b570791097650a945682f988df4
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-04 22:06:21 +0300

    пlkj;hkjghjhghg

--- js/task-06.js
+++ js/task-06.js
@@ -3,15 +3,9 @@ const greenBorder = document.querySelector('#validation-input.valid');
 const redBorder = document.querySelector('#validation-input.invalid');
 const symbolsNumber = document.querySelector('[data-length]');
 
-// function onInputBlur() {
-//   if (symbolsNumber !== 6) {
-//     inputData === redBorder;
-//   } else inputData === greenBorder;
-// }
-
-inputData.onchange = function () {
-  if (inputData.value.length != 6) {
-    inputData.style.borderColor = redBorder;
+inputData.addEventListener('blur', () => {
+  if ((String(inputData.value.trim()).length = symbolsNumber)) {
+    inputData.style.borderColor === greenBorder;
   }
-  inputData.style.borderColor = greenBorder;
-};
+  inputData.style.borderColor === redBorder;
+});
