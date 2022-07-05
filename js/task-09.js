1ddf39c880fabdef07ccc85065e439b5d437666c
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-05 16:22:50 +0300

    task 9 is done

--- js/task-09.js
+++ js/task-09.js
@@ -6,6 +6,5 @@ const spanColor = document.querySelector('.color');
 
 const buttonEl = document.querySelector('.change-color');
 buttonEl.addEventListener('click', () => {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  spanColor.textContent = 
+  spanColor.textContent = document.body.style.backgroundColor = getRandomHexColor();
 });
