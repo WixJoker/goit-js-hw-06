ec8f1dd4debdbd232ab156d0eeb6411e82e8de1c
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-05 16:20:26 +0300

    corrections are made

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 const enteredName = document.querySelector('#name-input');
 const spanValue = document.querySelector('#name-output');
 
-enteredName.addEventListener('input', event => {
+enteredName.addEventListener('input', () => {
   enteredName.value.trim() === ''
     ? (spanValue.textContent = 'Anonymous')
     : (spanValue.textContent = enteredName.value.trim());
--- js/task-09.js
+++ js/task-09.js
@@ -2,6 +2,10 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const buttonEl = document.querySelector('.change-color');
-buttonEl.addEventListener('click', () => {});
+const spanColor = document.querySelector('.color');
 
+const buttonEl = document.querySelector('.change-color');
+buttonEl.addEventListener('click', () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  spanColor.textContent = 
+});
