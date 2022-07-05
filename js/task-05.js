05f7da1493bbae0378ef04a453b5f8dc58716a0a
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-05 21:38:00 +0300

    task 5 corrected

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,5 @@ const enteredName = document.querySelector('#name-input');
 const spanValue = document.querySelector('#name-output');
 
 enteredName.addEventListener('input', () => {
-  enteredName.value.trim() === ''
-    ? (spanValue.textContent = 'Anonymous')
-    : (spanValue.textContent = enteredName.value.trim());
+  spanValue.textContent = enteredName.value.trim() === '' ? 'Anonymous' : enteredName.value.trim();
 });
