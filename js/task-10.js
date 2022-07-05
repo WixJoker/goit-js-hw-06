9d3a1bccb1880a8fe3385b600f6238ca7b58901b
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-05 20:06:41 +0300

    Corrections 2

--- js/task-10.js
+++ js/task-10.js
@@ -7,15 +7,18 @@ function getRandomHexColor() {
 // const divEl = document.createElement('div');
 const divBoxes = document.querySelector('div#boxes');
 
+const inputREf = document.querySelector('[type="number"]');
+
 const createButton = document.querySelector('[data-create]');
 const destroyButton = document.querySelector('[data-destroy]');
 
-// function createBoxes(amount) {
-//   amount = document.querySelector('[type="number"]');
-// }
+function createBoxes(amount) {
+  amount = inputREf.value;
+}
 
 createButton.addEventListener('click', () => {
-  divBoxes.insertAdjacentHTML('afterbegin', '<div></div>');
+  divBoxes.insertAdjacentHTML('afterbegin', '<div></div>')
+    = document.div.style.backgroundColor = getRandomHexColor();
 });
 
 destroyButton.addEventListener('click', () => {
