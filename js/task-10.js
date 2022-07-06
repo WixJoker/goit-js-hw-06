0d959b6319d1cced862005a2435e037ffbed85f5
WixJoker(andriilytvyn) <96374210+WixJoker@users.noreply.github.com>
2022-07-06 23:21:02 +0300

    tenth task is done

--- js/task-10.js
+++ js/task-10.js
@@ -1,24 +1,34 @@
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('#controls');
+const containerEl = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => createBoxes(inputEl.value));
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    size = size + 10;
+    const box = document.createElement('div');
+    box.style.backgroundColor = `${getRandomHexColor()}`;
+    box.style.width = size + 'px';
+    box.style.height = size + 'px';
+    box.classList.add('box-item');
+    containerEl.appendChild(box);
+  }
+  return containerEl;
+}
+
+function destroyBoxes() {
+  containerEl.innerHTML = '';
+  inputEl.value = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-const divBoxes = document.querySelector('div#boxes');
-
-const inputRef = document.querySelector('[type="number"]');
-
-const createButton = document.querySelector('[data-create]');
-const destroyButton = document.querySelector('[data-destroy]');
-
-function createBoxes(amount) {
-  amount = inputRef;
-}
-
-createButton.addEventListener('click', () => {
-  divBoxes.insertAdjacentHTML('afterbegin', `<div></div>`);
-});
-
-destroyButton.addEventListener('click', () => {
-  divBoxes.remove();
-});
